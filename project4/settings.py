259c2de43920de6ac9e0d437b3922e4df8984590
Wafa Qazi <wafa.qazi@outlook.com>
2022-12-11 22:53:24 -0500

    Update Django for whitenoise static

--- project4/settings.py
+++ project4/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
