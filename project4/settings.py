58215cf0ad878956bc052963666bfb79fa98fe39
wafas-repo <wafa.qazi@outlook.com>
2024-01-24 00:45:21 -0500

    updates for deploy

--- project4/settings.py
+++ project4/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['networkapp.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
